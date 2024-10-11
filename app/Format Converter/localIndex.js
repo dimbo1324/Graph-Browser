bbb66391d782252c7effe1f4c0ccaf4e61d41c6b
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-11 15:19:55 +0300

    ...

--- app/Format Converter/localIndex.js
+++ app/Format Converter/localIndex.js
@@ -1,20 +1,20 @@
 import { ConvertToJSON } from "../Format Converter/utils/main.js";
 
 export default class Converter {
+    #converter
     constructor(callback, sheet1Data = [], sheet2Data = []) {
         this.sheet1Data = sheet1Data;
         this.sheet2Data = sheet2Data;
         this.callback = callback;
+        this.#converter = new ConvertToJSON('upload', 'output', 'downloadSheet1', 'downloadSheet2');
         document.addEventListener('DOMContentLoaded', () => this.init());
     }
 
     init() {
-        const converter = new ConvertToJSON('upload', 'output', 'downloadSheet1', 'downloadSheet2');
-
         document.getElementById('processFileButton').addEventListener('click', () => {
-            converter.processFile((processedSheet1Data, processedSheet2Data) => {
-                this.sheet1Data = processedSheet1Data;
-                this.sheet2Data = processedSheet2Data;
+            this.#converter.processFile((processedSheet1Data, processedSheet2Data) => {
+                this.sheet1Data = this.#converter.getList1()
+                this.sheet2Data = this.#converter.getList2()
                 this.callback(this.sheet1Data, this.sheet2Data);
             });
         });
--- app/Format Converter/utils/main.js
+++ app/Format Converter/utils/main.js
@@ -4,6 +4,9 @@ import { JSONFormatter } from './json_formatter.js';
 import { DateTimeConverter } from './date_time_converter.js';
 
 export class ConvertToJSON {
+    #formattedData1 = [];
+    #formattedData2 = [];
+
     constructor(fileInputId, outputId, downloadLink1Id, downloadLink2Id) {
         this.fileReader = new FileReaderUtility(fileInputId);
         this.outputElement = document.getElementById(outputId);
@@ -19,25 +22,34 @@ export class ConvertToJSON {
             const sheet1Data = excelParser.parseSheet(1);
             const sheet2Data = excelParser.parseSheet(2);
 
-            const formattedData1 = this.jsonFormatter.formatData(sheet1Data);
-            const formattedData2 = this.jsonFormatter.formatData(sheet2Data);
+            this.#formattedData1 = this.jsonFormatter.formatData(sheet1Data);
+            this.#formattedData2 = this.jsonFormatter.formatData(sheet2Data);
 
-
-            this.updateDownloadLinks(formattedData1, formattedData2);
+            this.updateDownloadLinks(this.#formattedData1, this.#formattedData2);
 
             if (callback) {
-                callback(formattedData1, formattedData2);
+                callback(this.#formattedData1, this.#formattedData2);
             }
         });
     }
 
     updateDownloadLinks(formattedData1, formattedData2) {
-        const jsonBlob1 = new Blob([JSON.stringify(formattedData1)], { type: 'application/json' });
-        this.downloadLink1.href = URL.createObjectURL(jsonBlob1);
-        this.downloadLink1.style.display = 'block';
+        if (formattedData1 && formattedData2) {
+            const jsonBlob1 = new Blob([JSON.stringify(formattedData1)], { type: 'application/json' });
+            this.downloadLink1.href = URL.createObjectURL(jsonBlob1);
+            this.downloadLink1.style.display = 'block';
 
-        const jsonBlob2 = new Blob([JSON.stringify(formattedData2)], { type: 'application/json' });
-        this.downloadLink2.href = URL.createObjectURL(jsonBlob2);
-        this.downloadLink2.style.display = 'block';
+            const jsonBlob2 = new Blob([JSON.stringify(formattedData2)], { type: 'application/json' });
+            this.downloadLink2.href = URL.createObjectURL(jsonBlob2);
+            this.downloadLink2.style.display = 'block';
+        }
+    }
+
+    getList1() {
+        return this.#formattedData1;
+    }
+
+    getList2() {
+        return this.#formattedData2;
     }
 }
