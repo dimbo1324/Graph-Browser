006438468adfcf727994119d13f5aaab6e4999e4
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-16 14:52:10 +0300

    ...

--- app/script.js
+++ app/script.js
@@ -1,12 +1,9 @@
 import Chart from "./Chart/Chart.js";
-import { buttonZoom } from "./another/buttonZoom.js";
 
 const data1 = Array.from({ length: 30 }, (_, i) => ({
     date: new Date(2023, 0, i * 2),
     value: Math.random() * 100
 }));
-
-// Инициализация графика
 
 const chart = new Chart("#chart-container", data1)
 
