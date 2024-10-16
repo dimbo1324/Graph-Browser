fc53dc8784dcebef04654a30523b5c0e944a6356
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-16 15:30:22 +0300

    Необходимо привязать данные из реальных таблиц

--- app/script.js
+++ app/script.js
@@ -1,6 +1,6 @@
 import Chart from "./Chart/Chart.js";
 
-const data1 = Array.from({ length: 20000 }, (_, i) => ({
+const data1 = Array.from({ length: 233 }, (_, i) => ({
     date: new Date(2023, 0, 1, 0, 0, 0, i),
     value: Math.random() * 20000
 }));
