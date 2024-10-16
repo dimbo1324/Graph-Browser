34b8e13dcdcb78c6a2b9c63e275b344b35de5b1d
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-16 14:07:48 +0300

    Два графика уже есть

--- app/Chart/options.js
+++ app/Chart/options.js
@@ -3,7 +3,7 @@ const config = {
     margin: { top: 20, right: 20, bottom: 30, left: 40 },
     initialWidth: 8000,
     coefficientHeight: 17,
-    pointRadius: 2,
+    pointRadius: 4,
     strokeWidth: 0.75,
     zoomScaleExtent: [1, 10],
     zoomTranslateExtent: [[0, 0], [8000, 8000]],
--- app/script.js
+++ app/script.js
@@ -2,10 +2,18 @@ import Chart from "./Chart/Chart.js";
 
 
 // Пример данных
-const data = Array.from({ length: 222 }, (_, i) => ({
+const data1 = Array.from({ length: 222 }, (_, i) => ({
+    date: new Date(2023, 0, i * 2),
+    value: Math.random() * 100
+}));
+
+const data2 = Array.from({ length: 222 }, (_, i) => ({
     date: new Date(2023, 0, i),
     value: Math.random() * 100
 }));
 
+console.log(data1);
+
 // Инициализация графика
-new Chart("#chart-container", data);
+new Chart("#chart-container", data1, data2);
+
