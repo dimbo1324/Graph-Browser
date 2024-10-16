b722f1e06e315a3dc8d72735b8cb977c65a2bff4
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-16 10:17:10 +0300

    Еще один хороший вариант

--- app/InteractiveChart.js
+++ app/InteractiveChart.js
@@ -3,8 +3,8 @@ class ZoomableChart {
         this.container = container;
         this.data = data;
         this.margin = { top: 20, right: 20, bottom: 30, left: 40 };
-        this.width = 800 - this.margin.left - this.margin.right;
-        this.height = 400 - this.margin.top - this.margin.bottom;
+        this.width = 8000 - this.margin.left - this.margin.right;
+        this.height = this.width / 17
         this.initChart();
     }
 
@@ -34,28 +34,16 @@ class ZoomableChart {
         this.x = d3.scaleTime().range([0, this.width]);
         this.y = d3.scaleLinear().range([this.height, 0]);
 
+        // Настройка осей
         this.xAxis = this.svg.append("g")
             .attr("class", "x-axis")
-            .attr("transform", `translate(0,${this.height})`);
+            .attr("transform", `translate(${this.margin.left},${this.height + this.margin.top})`);
+
         this.yAxis = this.svg.append("g")
-            .attr("class", "y-axis");
+            .attr("class", "y-axis")
+            .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
 
-        this.xGrid = this.svg.append("g")
-            .attr("class", "x-grid")
-            .attr("transform", `translate(0,${this.height})`);
-        this.yGrid = this.svg.append("g")
-            .attr("class", "y-grid");
-
-        this.line = d3.line()
-            .x(d => this.x(d.date))
-            .y(d => this.y(d.value));
-
-        this.path = this.svg.append("path")
-            .attr("class", "line")
-            .style("fill", "none")
-            .style("stroke", "steelblue")
-            .style("stroke-width", 1.5);
-
+        // Добавляем Zoom
         this.zoom = d3.zoom()
             .scaleExtent([1, 10])
             .translateExtent([[0, 0], [this.width, this.height]])
@@ -81,72 +69,36 @@ class ZoomableChart {
         this.x.domain(d3.extent(this.data, d => d.date));
         this.y.domain([0, d3.max(this.data, d => d.value)]);
 
-        this.xGrid.call(d3.axisBottom(this.x)
-            .tickSize(-this.height)
-            .tickFormat(""))
-            .selectAll("line")
-            .style("stroke", "#e0e0e0");
+        // Рендерим оси
+        this.xAxis.call(d3.axisBottom(this.x));
+        this.yAxis.call(d3.axisLeft(this.y));
 
-        this.yGrid.call(d3.axisLeft(this.y)
-            .tickSize(-this.width)
-            .tickFormat(""))
-            .selectAll("line")
-            .style("stroke", "#e0e0e0");
-
-        this.path.datum(this.data)
-            .attr("d", this.line);
-
-        this.points = this.svg.selectAll(".point")
-            .data(this.data)
-            .join("circle")
-            .attr("class", "point")
-            .attr("cx", d => this.x(d.date))
-            .attr("cy", d => this.y(d.value))
-            .attr("r", 4)
-            .on("mouseover", (event, d) => this.showTooltip(event, d))
-            .on("mouseout", () => this.hideTooltip());
+        // Добавляем линию графика
+        this.path = this.g.append("path")
+            .datum(this.data)
+            .attr("class", "line")
+            .attr("d", d3.line()
+                .x(d => this.x(d.date))
+                .y(d => this.y(d.value)))
+            .style("fill", "none")
+            .style("stroke", "steelblue")
+            .style("stroke-width", 1.5);
     }
-
-    showTooltip(event, d) {
-        d3.select(this.container).append("div")
-            .attr("class", "tooltip")
-            .style("position", "absolute")
-            .style("left", `${event.pageX + 5}px`)
-            .style("top", `${event.pageY - 28}px`)
-            .style("background", "lightgrey")
-            .style("padding", "5px")
-            .style("border-radius", "4px")
-            .html(`Date: ${d.date.toLocaleDateString()}<br>Value: ${d.value}`);
-    }
-
-    hideTooltip() {
-        d3.select(this.container).selectAll(".tooltip").remove();
-    }
-
 
     zoomed(event) {
         const transform = event.transform;
         const newX = transform.rescaleX(this.x);
-        const newY = transform.rescaleY(this.y);
 
+        // Обновление оси X и положения линии графика при зуме
         this.xAxis.call(d3.axisBottom(newX));
-        this.yAxis.call(d3.axisLeft(newY));
-
-        this.xGrid.call(d3.axisBottom(newX)
-            .tickSize(-this.height)
-            .tickFormat(""));
-        this.yGrid.call(d3.axisLeft(newY)
-            .tickSize(-this.width)
-            .tickFormat(""));
-
-        this.path.attr("d", this.line.x(d => newX(d.date)).y(d => newY(d.value)));
-        this.points.attr("cx", d => newX(d.date)).attr("cy", d => newY(d.value));
+        this.path.attr("d", d3.line()
+            .x(d => newX(d.date))
+            .y(d => this.y(d.value)));
     }
 }
 
-
 // Пример данных
-const data = Array.from({ length: 222 }, (_, i) => ({
+const data = Array.from({ length: 22220 }, (_, i) => ({
     date: new Date(2023, 0, i),
     value: Math.random() * 100
 }));
