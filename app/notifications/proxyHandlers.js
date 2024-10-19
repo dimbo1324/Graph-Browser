f6d119dea1fe32e358d69627b0955bc1a3923775
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-19 09:30:20 +0300

    Проект с next.js

--- app/notifications/proxyHandlers.js
+++
@@ -1,8 +0,0 @@
-export const handlerForObservable = (observableInstance) => ({
-  set: (target, prop, value) => {
-    target[prop] = value;
-    observableInstance.notifyObservers(target, prop);
-    return true;
-  },
-});
-
