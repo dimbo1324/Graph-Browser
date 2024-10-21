fd3da52051390c799767ab585b9dba46359bf5cc
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-21 10:01:09 +0300

    ...

---
+++ app/charts/TimelineEditor.js
@@ -0,0 +1,16 @@
+export default class TimelineEditor {
+    #data
+    constructor(data) {
+        this.#data = data
+    }
+
+    edit() {
+        return this.#data.map((point) => {
+            const roundedTime = new Date(Math.round(new Date(point.x).getTime() / 1000) * 1000);
+            return {
+                x: roundedTime,
+                y: point.y
+            };
+        });
+    }
+}
